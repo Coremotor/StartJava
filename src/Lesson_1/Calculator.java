fe5a234c55171f7d05afd598477c1b0fe8163d18
Andrey Demakov <coremotor@outlook.com>
2022-12-30 18:11:03 +0300

    Lesson_1 - calculator

--- src/Lesson_1/Calculator.java
+++ src/Lesson_1/Calculator.java
@@ -3,6 +3,28 @@ package Lesson_1;
 public class Calculator {
 
     public static void main(String[] args) {
-//        +, -, *, /, ^, %
+        char action = '^';
+
+        int a = 5, b = 2, result = 0;
+        double resultDivision = 0;
+
+        switch (action) {
+            case '+' -> result = a + b;
+            case '-' -> result = a - b;
+            case '*' -> result = a * b;
+            case '/' -> resultDivision = (double) a / (double) b;
+            case '^' -> result = exponentiation(a, b);
+            case '%' -> result = a % b;
+        }
+
+        System.out.println(a + " " + action + " " + b + " = " + (action == '/' ? resultDivision : result));
+    }
+
+    private static int exponentiation(int a, int b) {
+        int temp = 1;
+        for (int i = 1; i <= b; i++) {
+            temp = temp * a;
+        }
+        return temp;
     }
 }
