9201159d3cbed75bf5b91bf7fa61efc40943237e
Andrey Demakov <coremotor@outlook.com>
2023-01-01 13:30:57 +0300

    Lesson_1 - game

---
+++ src/Lesson_1/Game.java
@@ -0,0 +1,21 @@
+package Lesson_1;
+
+import java.util.Random;
+
+public class Game {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int computerNumber = random.nextInt(1, 100), humanNumber;
+
+        do {
+            humanNumber = random.nextInt(1, 100);
+            if (computerNumber < humanNumber) {
+                System.out.println("Число " + humanNumber + " больше, чем загадал компьютер");
+            } else {
+                System.out.println("Число " + humanNumber + " меньше, чем загадал компьютер");
+            }
+        } while (computerNumber > humanNumber || computerNumber < humanNumber);
+
+        System.out.println("Вы победили!!!");
+    }
+}
